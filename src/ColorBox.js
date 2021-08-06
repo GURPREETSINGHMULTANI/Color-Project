06db229cfd1e7dab10e69048a5e3492759cdb699
Gurpreet Multani <GURPREETSINGHMULTANI@icloud.com>
2021-08-05 23:02:33 -0400

    Add Color Format Selecter and Material UI

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -18,6 +18,7 @@ class ColorBox extends Component {
     render() {
         const { name, background } = this.props;
         const { copied } = this.state;
+        console.log(background);
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className="ColorBox">
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import "./Navbar.css";
+import Select from "@material-ui/core/Select";
+import MenuItem from '@material-ui/core/MenuItem';
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { format: "hex" };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        this.setState({ format: e.target.value }, () => {
+            this.props.handleChange(this.state.format)
+        });
+    }
     render() {
         const { level, changeLevel } = this.props;
+        const { format } = this.state;
         return (
             <header className="Navbar">
                 <div className="logo">
@@ -25,6 +38,13 @@ class Navbar extends Component {
                             handleStyle={{ backgroundColor: 'green', outline: 'none', border: '2px solid green', boxShadow: 'none', width: '13px', height: '13px', marginTop: '-3px', marginLeft: '-7px' }}
                         />
                     </div>
+                </div>
+                <div className="select-container">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+                    </Select>
                 </div>
             </header>
         )
